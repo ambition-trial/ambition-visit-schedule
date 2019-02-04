862a20c4475372889e6f00c7add44440bfc3ea74
Erik van Widenfelt <ew2789@gmail.com>
2019-02-04 10:32:38 -0600

    Codecov, black (#2)

--- ambition_visit_schedule/visit_schedules/requisitions.py
+++ ambition_visit_schedule/visit_schedules/requisitions.py
@@ -15,44 +15,19 @@ from edc_visit_schedule import FormsCollection, Requisition
 
 
 requisitions_prn = FormsCollection(
+    Requisition(show_order=10, panel=fbc_panel, required=False, additional=False),
+    Requisition(show_order=20, panel=chemistry_panel, required=False, additional=False),
     Requisition(
-        show_order=10,
-        panel=fbc_panel,
-        required=False,
-        additional=False),
+        show_order=30, panel=chemistry_alt_panel, required=False, additional=False
+    ),
+    Requisition(show_order=40, panel=cd4_panel, required=False, additional=False),
     Requisition(
-        show_order=20,
-        panel=chemistry_panel,
-        required=False,
-        additional=False),
-    Requisition(
-        show_order=30,
-        panel=chemistry_alt_panel,
-        required=False,
-        additional=False
+        show_order=50, panel=viral_load_panel, required=False, additional=False
     ),
     Requisition(
-        show_order=40,
-        panel=cd4_panel,
-        required=False,
-        additional=False),
-    Requisition(
-        show_order=50,
-        panel=viral_load_panel,
-        required=False,
-        additional=False
+        show_order=60, panel=csf_chemistry_panel, required=False, additional=False
     ),
-    Requisition(
-        show_order=60,
-        panel=csf_chemistry_panel,
-        required=False,
-        additional=False
-    ),
-    Requisition(
-        show_order=70,
-        panel=csf_panel,
-        required=False,
-        additional=False),
+    Requisition(show_order=70, panel=csf_panel, required=False, additional=False),
     Requisition(
         show_order=80,
         panel=csf_pkpd_panel,
@@ -106,27 +81,13 @@ requisitions_prn = FormsCollection(
 )
 
 requisitions_d1 = FormsCollection(
+    Requisition(show_order=10, panel=fbc_panel, required=True, additional=False),
     Requisition(
-        show_order=10,
-        panel=fbc_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=20,
-        panel=chemistry_alt_panel,
-        required=True,
-        additional=False
+        show_order=20, panel=chemistry_alt_panel, required=True, additional=False
     ),
+    Requisition(show_order=30, panel=csf_panel, required=True, additional=False),
     Requisition(
-        show_order=30,
-        panel=csf_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=40,
-        panel=csf_chemistry_panel,
-        required=True,
-        additional=False
+        show_order=40, panel=csf_chemistry_panel, required=True, additional=False
     ),
     Requisition(
         show_order=50,
@@ -135,43 +96,18 @@ requisitions_d1 = FormsCollection(
         additional=False,
         site_ids=[get_site_id("blantyre")],
     ),
+    Requisition(show_order=60, panel=qpcr_csf_panel, required=True, additional=False),
+    Requisition(show_order=70, panel=csf_stop_panel, required=False, additional=False),
+    Requisition(show_order=80, panel=wb_panel, required=True, additional=False),
+    Requisition(show_order=90, panel=serum_panel, required=True, additional=False),
     Requisition(
-        show_order=60,
-        panel=qpcr_csf_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=70,
-        panel=csf_stop_panel,
-        required=False,
-        additional=False),
-    Requisition(
-        show_order=80,
-        panel=wb_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=90,
-        panel=serum_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=100,
-        panel=plasma_buffycoat_panel,
-        required=True,
-        additional=False
+        show_order=100, panel=plasma_buffycoat_panel, required=True, additional=False
     ),
     Requisition(
-        show_order=120,
-        panel=qpcr_blood_panel,
-        required=True,
-        additional=False
+        show_order=120, panel=qpcr_blood_panel, required=True, additional=False
     ),
     Requisition(
-        show_order=125,
-        panel=qpcr24_blood_panel,
-        required=True,
-        additional=False
+        show_order=125, panel=qpcr24_blood_panel, required=True, additional=False
     ),
     Requisition(
         show_order=130,
@@ -184,51 +120,25 @@ requisitions_d1 = FormsCollection(
 )
 
 requisitions_d3 = FormsCollection(
+    Requisition(show_order=20, panel=chemistry_panel, required=True, additional=False),
     Requisition(
-        show_order=20,
-        panel=chemistry_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=40,
-        panel=plasma_buffycoat_panel,
-        required=True,
-        additional=False
+        show_order=40, panel=plasma_buffycoat_panel, required=True, additional=False
     ),
-    Requisition(
-        show_order=50,
-        panel=qpcr_blood_panel,
-        required=True,
-        additional=False),
+    Requisition(show_order=50, panel=qpcr_blood_panel, required=True, additional=False),
     name="requisitions_day3",
 )
 
 requisitions_d5 = FormsCollection(
-    Requisition(
-        show_order=10,
-        panel=chemistry_panel,
-        required=True,
-        additional=False),
+    Requisition(show_order=10, panel=chemistry_panel, required=True, additional=False),
     name="requisitions_default",
 )
 
 requisitions_d7 = FormsCollection(
+    Requisition(show_order=10, panel=fbc_panel, required=True, additional=False),
     Requisition(
-        show_order=10,
-        panel=fbc_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=20,
-        panel=chemistry_alt_panel,
-        required=True,
-        additional=False
+        show_order=20, panel=chemistry_alt_panel, required=True, additional=False
     ),
-    Requisition(
-        show_order=40,
-        panel=csf_panel,
-        required=True,
-        additional=False),
+    Requisition(show_order=40, panel=csf_panel, required=True, additional=False),
     Requisition(
         show_order=50,
         panel=csf_pkpd_panel,
@@ -236,27 +146,12 @@ requisitions_d7 = FormsCollection(
         additional=False,
         site_ids=[get_site_id("blantyre")],
     ),
+    Requisition(show_order=60, panel=qpcr_csf_panel, required=True, additional=False),
+    Requisition(show_order=70, panel=csf_stop_panel, required=False, additional=False),
     Requisition(
-        show_order=60,
-        panel=qpcr_csf_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=70,
-        panel=csf_stop_panel,
-        required=False,
-        additional=False),
-    Requisition(
-        show_order=80,
-        panel=plasma_buffycoat_panel,
-        required=True,
-        additional=False
+        show_order=80, panel=plasma_buffycoat_panel, required=True, additional=False
     ),
-    Requisition(
-        show_order=90,
-        panel=qpcr_blood_panel,
-        required=True,
-        additional=False),
+    Requisition(show_order=90, panel=qpcr_blood_panel, required=True, additional=False),
     Requisition(
         show_order=95,
         panel=pk_plasma_panel_t0,
@@ -275,40 +170,21 @@ requisitions_d7 = FormsCollection(
 )
 
 requisitions_d10 = FormsCollection(
-    Requisition(
-        show_order=20,
-        panel=chemistry_panel,
-        required=True,
-        additional=False),
+    Requisition(show_order=20, panel=chemistry_panel, required=True, additional=False),
     name="requisitions_day10",
 )
 
 requisitions_d12 = FormsCollection(
-    Requisition(
-        show_order=20,
-        panel=chemistry_panel,
-        required=True,
-        additional=False),
+    Requisition(show_order=20, panel=chemistry_panel, required=True, additional=False),
     name="requisitions_day12",
 )
 
 requisitions_d14 = FormsCollection(
+    Requisition(show_order=10, panel=fbc_panel, required=True, additional=False),
     Requisition(
-        show_order=10,
-        panel=fbc_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=30,
-        panel=chemistry_alt_panel,
-        required=True,
-        additional=False
+        show_order=30, panel=chemistry_alt_panel, required=True, additional=False
     ),
-    Requisition(
-        show_order=40,
-        panel=csf_panel,
-        required=True,
-        additional=False),
+    Requisition(show_order=40, panel=csf_panel, required=True, additional=False),
     Requisition(
         show_order=50,
         panel=csf_pkpd_panel,
@@ -316,41 +192,19 @@ requisitions_d14 = FormsCollection(
         additional=False,
         site_ids=[get_site_id("blantyre")],
     ),
+    Requisition(show_order=60, panel=qpcr_csf_panel, required=True, additional=False),
+    Requisition(show_order=70, panel=csf_stop_panel, required=False, additional=False),
     Requisition(
-        show_order=60,
-        panel=qpcr_csf_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=70,
-        panel=csf_stop_panel,
-        required=False,
-        additional=False),
-    Requisition(
-        show_order=80,
-        panel=plasma_buffycoat_panel,
-        required=True,
-        additional=False
+        show_order=80, panel=plasma_buffycoat_panel, required=True, additional=False
     ),
-    Requisition(
-        show_order=90,
-        panel=qpcr_blood_panel,
-        required=True,
-        additional=False),
+    Requisition(show_order=90, panel=qpcr_blood_panel, required=True, additional=False),
     name="requisitions_day14",
 )
 
 requisitions_w4 = FormsCollection(
+    Requisition(show_order=10, panel=fbc_panel, required=True, additional=False),
     Requisition(
-        show_order=10,
-        panel=fbc_panel,
-        required=True,
-        additional=False),
-    Requisition(
-        show_order=20,
-        panel=chemistry_alt_panel,
-        required=True,
-        additional=False
+        show_order=20, panel=chemistry_alt_panel, required=True, additional=False
     ),
     name="requisitions_week4",
 )
